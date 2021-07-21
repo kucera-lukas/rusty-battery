c5166daeb6880523bf1e6b2b0e53ac1bda06728e
lukas-kucera <lukas.kucera.g@protonmail.com>
2021-07-21 19:37:46 +0200

    clean up main module

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,8 @@
-use std::process::Command;
-
-use regex;
-use std::io;
-
-use notify_rust::{Hint, Notification};
-
-fn upower_command() -> Result<String, io::Error> {
-    let output = Command::new("upower")
-        .args(["-i", "/org/freedesktop/UPower/devices/battery_BAT1"])
-        .output()?;
-    Ok(String::from_utf8_lossy(&output.stdout).into_owned())
-}
-
-fn battery_percentage(output: &String) -> Option<i32> {
-    let re = regex::Regex::new(r".*percentage:\s+?(\d+)%.*").unwrap();
-    let caps = re.captures(output)?;
-    let result = match caps.get(1)?.as_str().parse() {
-        Ok(n) => Some(n),
-        Err(..) => None,
-    };
-
-    result
-}
+mod battery;
+mod notification;
 
 fn main() {
-    let output = upower_command().expect("cannot get battery data");
-    let percentage = battery_percentage(&output).expect("could not process battery output");
+    let percentage = battery::get_battery_percentage().expect("could not process battery output");
 
-    Notification::new()
-        .summary("Charge limit warning")
-        .body(&format!(
-            "Battery percentage already at {}%, you might want to unplug your charger",
-            percentage.to_string()
-        ))
-        .icon("clion")
-        .appname("rusty-battery")
-        .hint(Hint::Category("device".to_owned()))
-        .timeout(0)
-        .show()
-        .unwrap();
+    notification::notify(percentage);
 }
