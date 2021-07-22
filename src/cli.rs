2c171e13b2c7c1197c8af988efd1ec9ba173c48e
lukas-kucera <lukas.kucera.g@protonmail.com>
2021-07-22 20:32:37 +0200

    switch to structopt

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,16 @@
-use clap::{AppSettings, Clap};
+use structopt::StructOpt;
 
 /// Tool to help you care about your device's battery health
 /// by showing a desktop notification whenever battery percentage
 /// exceeds a given threshold.
-#[derive(Clap)]
-#[clap(version = "0.1.0", author = "Lukas Kucera <lukas.kucera.g@gmail.com>")]
-#[clap(setting = AppSettings::ColoredHelp)]
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rusty-battery")]
 pub struct Opts {
     /// Battery charge threshold
-    #[clap(short, long)]
+    #[structopt(short, long, default_value = "80")]
     pub threshold: i32,
 }
 
 pub fn parse() -> Opts {
-    Opts::parse()
+    Opts::from_args()
 }
