d4a687d60c3bfa772ed41d412714e976f08fef83
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-09-22 18:40:44 +0200

    update `error.rs`

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,14 @@ use thiserror::Error;
 use battery::State;
 
 #[derive(Error, Debug)]
+pub enum AppError {
+    #[error("something went wrong while fetching battery information: {}", .0)]
+    Battery(#[from] BatteryError),
+    #[error("something went wrong with user notification: {}", .0)]
+    Notification(#[from] NotificationError),
+}
+
+#[derive(Error, Debug)]
 pub enum BatteryError {
     #[error("could not find any battery device")]
     DeviceError,
@@ -13,9 +21,7 @@ pub enum BatteryError {
 }
 
 #[derive(Error, Debug)]
-pub enum EventError {
-    #[error("something went wrong while getting battery information: {}", .0)]
-    Battery(#[from] BatteryError),
+pub enum NotificationError {
     #[error("something went wrong while showing a desktop notification: {}", .0)]
-    Notification(#[from] notify_rust::error::Error),
+    Desktop(#[from] notify_rust::error::Error),
 }
