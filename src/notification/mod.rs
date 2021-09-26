eb2c52eedd18664f72fb13268f3b457aa0b68f7a
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-09-26 22:33:29 +0200

    test `notification/mod.rs`

--- src/notification/mod.rs
+++ src/notification/mod.rs
@@ -1,16 +1,51 @@
-pub mod desktop;
+mod desktop;
 
-pub struct Notifier<D: desktop::ProvideDesktopNotification> {
-    pub desktop: D,
+#[derive(Debug)]
+pub struct Notifier {
+    pub desktop: desktop::DesktopNotifier,
 }
 
-impl<D> Notifier<D>
-where
-    D: desktop::ProvideDesktopNotification,
-{
-    pub fn new(desktop_notifier: D) -> Self {
+impl Notifier {
+    /// Create a new `Notifier` instance.
+    pub const fn new(threshold: u8) -> Self {
         Self {
-            desktop: desktop_notifier,
+            desktop: desktop::DesktopNotifier::new(threshold),
         }
+    }
+}
+
+mod std_fmt_impls {
+    use std::fmt;
+
+    use super::Notifier;
+
+    impl fmt::Display for Notifier {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "desktop: {}", self.desktop)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fmt::Display;
+
+    #[test]
+    fn test_notifier_new() {
+        let threshold = 0;
+        let notifier = Notifier::new(threshold);
+
+        assert_eq!(notifier.desktop.threshold, threshold);
+    }
+
+    #[test]
+    fn test_notifier_display() {
+        let threshold = 0;
+        let notifier = Notifier::new(threshold);
+
+        let display = format!("{}", notifier);
+
+        assert_eq!(display, "desktop: threshold: 0, handle: None")
     }
 }
