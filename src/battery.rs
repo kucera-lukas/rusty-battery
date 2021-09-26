975c32bd057582c4592be121ac3e60cc8fdb2e29
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-09-26 22:32:37 +0200

    test `battery.rs`

--- src/battery.rs
+++ src/battery.rs
@@ -1,114 +1,133 @@
 //! Battery information.
 
-use std::result;
-
-use crate::error::BatteryError;
-
-pub type Result<T> = result::Result<T, BatteryError>;
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum BatteryState {
     Charging,
     Discharging,
 }
 
-pub trait ProvideBatteryData {
-    /// Return current battery percentage.
-    fn percentage(&self) -> u8;
-    /// Return current battery state as the `BatteryState` enum.
-    fn state(&self) -> Result<BatteryState>;
-}
-
 #[derive(Debug)]
-pub struct BatteryDataProvider {
-    device: battery::Battery,
-}
-
-impl ProvideBatteryData for BatteryDataProvider {
-    #[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
-    fn percentage(&self) -> u8 {
-        let percentage = self
-            .device
-            .state_of_charge()
-            .get::<battery::units::ratio::percent>()
-            .trunc() as u8;
-
-        log::debug!("current battery percentage = {}%", percentage);
-
-        percentage
-    }
-
-    fn state(&self) -> Result<BatteryState> {
-        let state = self.device.state();
-
-        let result = match state {
-            battery::State::Charging | battery::State::Full => {
-                Ok(BatteryState::Charging)
-            }
-            battery::State::Discharging | battery::State::Empty => {
-                Ok(BatteryState::Discharging)
-            }
-            _ => Err(BatteryError::UnknownState { state }),
-        };
-
-        if let Ok(state) = &result {
-            log::debug!("current battery state = {}", state);
-        };
-
-        result
-    }
-}
-
-impl BatteryDataProvider {
-    /// Create a new instance of `BatteryDataProvider` via the `battery::Battery` device object.
-    pub fn new() -> Result<Self> {
-        let manager = battery::Manager::new()?;
-        let device = manager
-            .batteries()?
-            .next()
-            .ok_or(BatteryError::DeviceError)??;
-
-        Ok(Self { device })
-    }
-}
-
-#[derive(Debug)]
-pub struct BatteryInfo<B: ProvideBatteryData> {
+pub struct BatteryInfo {
     pub percentage: u8,
     pub state: BatteryState,
 
-    provider: B,
+    device: battery::Battery,
 }
 
-impl<B> BatteryInfo<B>
-where
-    B: ProvideBatteryData,
-{
-    /// Construct a new `Info` instance.
-    pub fn new(battery_provider: B) -> Result<Self> {
-        let info = Self {
-            percentage: battery_provider.percentage(),
-            state: battery_provider.state()?,
-            provider: battery_provider,
-        };
-        Ok(info)
+impl BatteryInfo {
+    /// Construct a new `BatteryInfo` instance.
+    pub fn new(model: Option<&str>) -> Self {
+        let device = device(model);
+
+        Self {
+            percentage: percentage(&device),
+            state: state(&device),
+            device,
+        }
     }
 
     /// Update attributes to current battery values.
-    pub fn refresh(&mut self) -> Result<&mut Self> {
-        self.percentage = self.provider.percentage();
-        self.state = self.provider.state()?;
+    pub fn refresh(&mut self) -> &mut Self {
+        self.refresh_percentage().refresh_state()
+    }
 
-        log::info!("refreshed: {}", self);
+    /// Update battery percentage to current value.
+    fn refresh_percentage(&mut self) -> &mut Self {
+        self.percentage = percentage(&self.device);
+        self
+    }
 
-        Ok(self)
+    /// Update `BatteryState` to current state.
+    fn refresh_state(&mut self) -> &mut Self {
+        self.state = state(&self.device);
+        self
+    }
+}
+
+/// Return `Iterator` over all available `battery::Battery` devices.
+fn batteries() -> impl Iterator<Item = battery::Battery> {
+    battery::Manager::new()
+        // this never panics on linux
+        .unwrap()
+        .batteries()
+        .expect("failed to read the /sys/class/power_supply directory")
+        .take_while(Result::is_ok)
+        .flatten()
+}
+
+/// Return an instance of `battery::Battery`.
+///
+/// # Panics
+///
+/// If no `battery::Battery` was found.
+/// If `model` was `None` and more than one `battery::Battery` exists.
+/// If `model` was `Some` and the method `model` of each `battery::Battery` device did not match it.
+fn device(model: Option<&str>) -> battery::Battery {
+    let mut batteries = batteries();
+
+    match model {
+        None => match batteries.next() {
+            None => panic!("no battery devices found"),
+            Some(battery) => match batteries.next() {
+                // only one battery device found so specifying model would not change anything
+                // this should be the most common case
+                None => battery,
+                Some(_) => panic!(
+                    "multiple battery devices found, please specify which model to use"
+                )
+            },
+        },
+        Some(model) => match batteries
+            .find(|battery| battery.model() == Some(model))
+        {
+            None => panic!(
+                "no battery device with model = \"{}\" was found",
+                model
+            ),
+            Some(battery) => battery,
+        },
+    }
+}
+
+/// Return current battery percentage of the given `battery::Battery` device.
+#[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
+fn percentage(device: &battery::Battery) -> u8 {
+    let percentage = device
+        .state_of_charge()
+        .get::<battery::units::ratio::percent>()
+        .trunc() as u8;
+
+    log::debug!("current battery percentage = {}%", percentage);
+
+    percentage
+}
+
+/// Return current `BatterState` of the given `battery::Battery` device.
+///
+/// # Panics
+///
+/// If the `state` method of the given `battery::Battery` device
+/// returns `battery::State::Unknown`.
+fn state(device: &battery::Battery) -> BatteryState {
+    let state = device.state();
+
+    log::debug!("current battery state = {}", state);
+
+    match state {
+        battery::State::Charging | battery::State::Full => {
+            BatteryState::Charging
+        }
+        battery::State::Discharging | battery::State::Empty => {
+            BatteryState::Discharging
+        }
+        _ => panic!("unknown battery state: {}", state),
     }
 }
 
 mod std_fmt_impls {
     use std::fmt;
 
-    use super::{BatteryInfo, BatteryState, ProvideBatteryData};
+    use super::{BatteryInfo, BatteryState};
 
     impl fmt::Display for BatteryState {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -119,10 +138,7 @@ mod std_fmt_impls {
         }
     }
 
-    impl<B> fmt::Display for BatteryInfo<B>
-    where
-        B: ProvideBatteryData,
-    {
+    impl fmt::Display for BatteryInfo {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             write!(
                 f,
@@ -130,5 +146,28 @@ mod std_fmt_impls {
                 self.percentage, self.state,
             )
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_battery_state_charging_display() {
+        let state = BatteryState::Charging;
+
+        let display = format!("{}", state);
+
+        assert_eq!(display, "Charging")
+    }
+
+    #[test]
+    fn test_battery_state_discharging_display() {
+        let state = BatteryState::Discharging;
+
+        let display = format!("{}", state);
+
+        assert_eq!(display, "Discharging")
     }
 }
