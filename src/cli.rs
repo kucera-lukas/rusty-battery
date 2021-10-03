0e97859c31e428b72f980f3e1368c73ca32f4be0
lukas-kucera <kucera-lukas@users.noreply.github.com>
2021-10-03 22:54:50 +0200

    new cli options

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use structopt::StructOpt;
 #[structopt(name = "rusty-battery")]
 pub struct Opts {
     /// Activates verbose mode
-    #[structopt(short, long, parse(from_occurrences))]
+    #[structopt(short, long, parse(from_occurrences), global = true)]
     pub verbose: u8,
 
     #[structopt(subcommand)]
@@ -21,8 +21,22 @@ pub enum Command {
         threshold: u8,
 
         /// Battery model name
+        ///
+        /// If this value is omitted and only battery device is found for the current device,
+        /// that one will be used.
+        ///
+        /// Otherwise, please use the `batteries` subcommand to get a list of all battery devices
+        /// to get the model of the wanted battery device which should be monitored.
         #[structopt(short, long)]
         model: Option<String>,
+
+        /// KDE Connect device names
+        ///
+        /// If this value is not present, KDE Connect will not be used.
+        ///
+        /// If this value is empty, all of the KDE Connect devices will be pinged.
+        #[structopt(long = "kde-connect")]
+        kde_connect_names: Option<Vec<String>>,
     },
     /// List all available batteries of the current device.
     Batteries,
